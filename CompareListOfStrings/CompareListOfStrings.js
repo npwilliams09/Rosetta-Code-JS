666826135b48b394bdf19f9279edea3b6f9832a8
Nathan Williams <npwilliams09@gmail.com>
2018-07-22 12:43:52 +1000

    updated compareList solution

--- CompareListOfStrings/CompareListOfStrings.js
+++ CompareListOfStrings/CompareListOfStrings.js
@@ -1,47 +1,28 @@
 function allEqual (arr) {
-  if (arr.length === 0){
+  if (arr.length <= 1){
     return true;
   }
-  let resArr = [];
+  let prev = arr[0];
   for (let i = 0; i < arr.length; i++){
-    let tally = 0;
-    for (let j = 0; j < arr[i].length; j++){
-      tally += arr[i].charCodeAt(j);
+    if (prev !== arr[i]){
+        return false;
     }
-    resArr.push(tally);
+    prev = arr[i];
   }
-  let total = resArr.reduce((acc,curr)=>acc+curr);
-  if (total%resArr[0] === 0){
-    return true;
-  }
-  return false;
+  return true;
 }
 
 function azSorted (arr) {
   // Good luck!
-  if (arr.length === 0){
+  if (arr.length <= 1){
     return true;
   }
-  for (let i = 0; i < arr.length-1; i++){
-    let shortestLen = 0;
-    let elem1 = arr[i];
-    let elem2 = arr[i+1];
-    if (elem1.length <= elem2.length){
-      shortestLen = elem1.length;
-    }else{
-      shortestLen = elem2.length;
-    }
-    for (let j = 0; j < shortestLen; j++){
-      if(elem1.charCodeAt(j) === elem2.charCodeAt(j) && j === shortestLen-1){
+  let prev = arr[0];
+  for (let i = 1; i < arr.length; i++){
+    if (prev >= arr[i]){
         return false;
-      }else if (elem1.charCodeAt(j) === elem2.charCodeAt(j)){
-        continue;
-      }else if (elem1.charCodeAt(j) < elem2.charCodeAt(j)){
-        break;
-      }else{
-        return false;
-      }
     }
+    prev = arr[i];
   }
   return true;
 }
